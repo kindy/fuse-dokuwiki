e856d05fbbbb85c3495c1b0535e2d513ea9b6d8d
Kindy Lin <kindy.lin@gmail.com>
2018-08-22 23:24:03 +0800

    update doc

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='dokuwiki-fuse',
-    version='0.0.1',
+    version='0.0.2',
     description='Fuse for Dokuwiki',
     long_description=long_description,
     long_description_content_type='text/markdown',
